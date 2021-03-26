238f9c33880a5ea666c6f9e4395028a7ded0b889
Eugene Pereverzev <EugenePereverzev@mail.ru>
2021-03-27 00:55:36 +0300

    Добавил папку 4 урок. Дополнил решения во 2 урок

---
+++ lesson-4/script.js
@@ -0,0 +1,32 @@
+
+/*
+const media = [{},{}];
+media
+.filter({type} => type ==='tvshow')
+.sort((a, b) => {a.year - b.year || ()=> {} || a.episodes.episodenumber - a.episodes.episodenumber});
+*/
+
+const users = ['Артём','Борис','Вова'];
+
+console.log ('users', users);
+
+const fruits = [];
+
+fruits[2] = 'Яблоко';
+fruits[5] = 'Банан';
+
+console.log ('fruits', fruits);
+
+const userObjs = [{
+    name : 'Артём',
+    age : 23,
+},{
+    name : 'Борис',
+    age : 18,
+},{
+    name : 'Вова',
+    age : 31,  
+}];
+
+
+console.log ('userObjs', userObjs);
