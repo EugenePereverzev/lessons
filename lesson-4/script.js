30ed71d4b26e05a079e6b21c6f7efb8c9a0d167e
Eugene Pereverzev <EugenePereverzev@mail.ru>
2021-03-28 21:17:16 +0300

    Добавил решения 4 урока.

--- lesson-4/script.js
+++ lesson-4/script.js
@@ -28,5 +28,84 @@ const userObjs = [{
     age : 31,  
 }];
 
-
 console.log ('userObjs', userObjs);
+
+let {name: vName, age: vAge} = userObjs[1];
+
+console.log ('Name:', vName, 'Age:', vAge);
+
+const media = [{
+    name: 'Титаник',
+    type: 'movie',
+    year:'1997',
+},{
+    name: 'Приключения Шерлока Холмса и доктора Ватсона',
+    type: 'tvshow',
+    year:'1979',
+    episodes: [{
+        name: 'Король шантажа',
+        episodeNumber: 3,
+    },{
+        name: 'Знакомство',
+        episodeNumber: 1,
+    },{
+        name: 'Охота на тигра',
+        episodeNumber: 5,
+    },{
+        name: 'Смертельная схватка',
+        episodeNumber: 4,
+    },{
+        name: 'Кровавая надпись',
+        episodeNumber: 2,
+    }],
+},{
+    name: 'Солярис',
+    type: 'movie',
+    year:'1972',
+},{
+    name: 'Семнадцать мгновений весны',
+    type: 'tvshow',
+    year:'1971',
+    episodes: [{
+        name: 'Серия 12',
+        episodeNumber: 12,
+    },{
+        name: 'Серия 1',
+        episodeNumber: 1,
+    },{
+        name: 'Серия 10',
+        episodeNumber: 10,
+    },{
+        name: 'Серия 11',
+        episodeNumber: 11,
+    },{
+        name: 'Серия 2',
+        episodeNumber: 2,
+    }],
+}];
+
+let episodes = media
+    .filter (({type}) => type === 'tvshow')
+    .sort (({year: y1}, {year: y2}) => y1 - y2)
+    .map (({episodes: ep}) => ep.sort (({episodeNumber: en1}, {episodeNumber: en2}) => en1 - en2).map (({name}) => name))
+    .flat ()
+    ;
+
+console.log (episodes);
+
+// Домашнее задание
+function myReduce (arr, func) {
+   // return arr.map ((value, index, array) => func (value, index, array));
+   return 'impossible';
+};
+
+function myMap (arr, func) {
+    return arr.reduce ((a, value, index, array) => a.concat (func (value, index, array)), []);
+};
+// Функция для тестирования
+function some (value, index, array) {
+    return `${value.name}: ${value.age}`;
+};
+
+console.log ('myMap', myMap (userObjs, some));
+console.log ('.map',  userObjs.map (some));
